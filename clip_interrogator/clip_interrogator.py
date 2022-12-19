10d36752028f6ee8a3c8f254f0c2b58d2c78b402
minamikik <mia@sync.dev>
2022-12-20 02:16:33 +0900

    Update clip_interrogator.py

--- clip_interrogator/clip_interrogator.py
+++ clip_interrogator/clip_interrogator.py
@@ -7,6 +7,7 @@ import os
 import pickle
 import time
 import torch
+import hashlib
 
 from dataclasses import dataclass
 from models.blip import blip_decoder, BLIP_Decoder
@@ -41,7 +42,6 @@ class Config:
     data_path: str = os.path.join(os.path.dirname(__file__), 'data')
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
     flavor_intermediate_count: int = 2048
-    is_artists: bool = True
     alt_flavors_path: str = None
     quiet: bool = False # when quiet progress bars are not shown
 
@@ -57,15 +57,32 @@ class Interrogator():
             blip_path = os.path.dirname(inspect.getfile(blip_decoder))
             configs_path = os.path.join(os.path.dirname(blip_path), 'configs')
             med_config = os.path.join(configs_path, 'med_config.json')
+            cache_model_path = os.path.join(config.cache_path, 'model_large_caption.pth')
+            if os.path.exists(cache_model_path):
+                model_path = cache_model_path
+                with open(cache_model_path, 'rb') as f:
+                    model_hash = hashlib.md5(f.read()).hexdigest()
+                    print(f'Cached BLIP model hash: {model_hash}')
+                    if not model_hash == 'b78e0b7488c83ba75d58f93f79e885b6':
+                        print(f'Cached BLIP model is out of date, downloading new model from {config.blip_model_url}...')
+                        model_path = config.blip_model_url
+                    else:
+                        print(f'Using cached BLIP model from {cache_model_path}')
+            else:
+                print(f'No cached BLIP model found, downloading new model from {config.blip_model_url}...')
+                model_path = config.blip_model_url
+            print(f'Loading BLIP model from {model_path}')
             blip_model = blip_decoder(
-                pretrained=config.blip_model_url, 
+                pretrained=model_path, 
                 image_size=config.blip_image_eval_size, 
                 vit='large', 
                 med_config=med_config
             )
+            print(f'Loaded BLIP blip_decoder')
             blip_model.eval()
             blip_model = blip_model.to(config.device)
             self.blip_model = blip_model
+            print(f'Initialized BLIP model')
         else:
             self.blip_model = config.blip_model
 
@@ -100,9 +117,7 @@ class Interrogator():
         trending_list.extend(["featured on "+site for site in sites])
         trending_list.extend([site+" contest winner" for site in sites])
 
-        raw_artists = []
-        if config.is_artists:
-            raw_artists = _load_list(config.data_path, 'artists.txt')
+        raw_artists = _load_list(config.data_path, 'artists.txt')
         artists = [f"by {a}" for a in raw_artists]
         artists.extend([f"inspired by {a}" for a in raw_artists])
 
@@ -157,6 +172,13 @@ class Interrogator():
             image_features = self.clip_model.encode_image(images)
             image_features /= image_features.norm(dim=-1, keepdim=True)
         return image_features
+
+    def interrogate_one(self, image: Image, seed_path) -> str:
+        image_features = self.image_to_features(image)
+        seed = _load_list(os.path.dirname(seed_path), os.path.basename(seed_path))
+        labels = LabelTable(seed, os.path.basename(seed_path), self.clip_model, self.tokenize, self.config)
+        top = labels.rank(image_features, 1)[0]
+        return top
 
     def interrogate_classic(self, image: Image, max_flavors: int=3) -> str:
         caption = self.generate_caption(image)
